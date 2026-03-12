0911eae6f3d570a317319f26af82209d8c2ffd7a
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-03-12 18:39:10 +0530

    DashboardLayout updated

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -4,19 +4,28 @@ import Navbar from './Navbar'
 import SideMenu from './SideMenu'
 
 const DashboardLayout = ({children,activeMenu}) => {
-    const {currentUser} = useSelector((state)=>state.user)
+  const {currentUser} = useSelector((state)=>state.user)
 
   return (
-    <div className='min-h-screen flex flex-col'>
-        <Navbar activeMenu={activeMenu}/>
-        {currentUser && (
-            <div className='flex flex-1'>
-                <div className='max-[1080px]:hidden'>
-                    <SideMenu activeMenu={activeMenu}/>
-                </div>
-                <div className='grow mx-5'>{children}</div>
-            </div>
-        )}
+    <div className='min-h-screen flex flex-col overflow-x-hidden'>
+      
+      <Navbar activeMenu={activeMenu}/>
+
+      {currentUser && (
+        <div className='flex flex-1 w-full'>
+
+          <div className='max-[1080px]:hidden'>
+            <SideMenu activeMenu={activeMenu}/>
+          </div>
+
+          {/* main content */}
+          <div className='flex-1 w-full px-3 sm:px-5 lg:mx-5'>
+            {children}
+          </div>
+
+        </div>
+      )}
+
     </div>
   )
 }
